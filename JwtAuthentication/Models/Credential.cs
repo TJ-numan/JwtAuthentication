0bd2ea7130622519558ea655c4d65958044ce85e
TJ-numan <34963162+TJ-numan@users.noreply.github.com>
2026-04-26 22:39:26 +0600

    commited to file to check github action

--- JwtAuthentication/Models/Credential.cs
+++ JwtAuthentication/Models/Credential.cs
@@ -5,6 +5,7 @@ namespace JwtAuthentication.Models
     [Serializable]
     public class Credential
     {
+        // we will add more permaeter alter to the credentials
         public string UserName { get; set; }
         public string Password { get; set; }
     }
