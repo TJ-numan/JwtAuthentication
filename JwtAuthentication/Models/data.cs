58a829cc86c8357576cce4c2979fed213a2efa8c
TJ-numan <34963162+TJ-numan@users.noreply.github.com>
2026-04-26 22:50:41 +0600

    new model added

---
+++ JwtAuthentication/Models/data.cs
@@ -0,0 +1,13 @@
+﻿namespace JwtAuthentication.Models
+{
+    public class data
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string subject { get; set; }
+        public string body { get; set; }
+            
+        public string contentType { get; set; }
+
+    }
+}
